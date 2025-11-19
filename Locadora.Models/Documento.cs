45464d3ddf349d03e68f2190023b6c2b585f635a
fesperli <fesperli@gmail.com>
2025-11-19 16:21:20 -0300

    Adicionar arquivos de projeto.

---
+++ Locadora.Models/Documento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Documento
+    {
+        public int DocumentoID { get; private set; }
+        public int ClienteID { get; private set; }
+        public string TipoDocumento { get; private set; }
+        public string Numero { get; private set; }
+        public DateTime DataEmissao { get; private set; }
+        public DateTime DataValidade { get; private set; }
+
+        public Documento (int clienteID, string tipoDocumento, string numero, DateTime dataEmissao, DateTime dataValidade)
+        {
+            ClienteID = clienteID;
+            TipoDocumento = tipoDocumento;
+            Numero = numero;
+            DataEmissao = dataEmissao;
+            DataValidade = dataValidade;
+        }
+
+        public override string ToString()
+        {
+            return $"TipoDocumento: {TipoDocumento}\nNumero: {Numero}\nDataEmissao: {DataEmissao}\nDataValidade: {DataValidade}\n";
+        }
+    }
+}
---
+++ Locadora.View/Program.cs
@@ -0,0 +1,45 @@
+﻿using Locadora.Controller;
+using Locadora.Models;
+
+
+//Cliente cliente = new Cliente("Ted", "aquele@urso.com");
+
+//Documento documento = new Documento(
+//    1,
+//    "RG",
+//    "123456789",
+//    new DateTime(2020, 1, 15),
+//    new DateTime(2030, 1, 15)
+//);
+
+//Console.WriteLine(cliente);
+//Console.WriteLine("---------------------------------------------");
+
+var clienteController = new ClienteController();
+//try
+//{
+//    clienteController.AdicionarCliente(cliente);
+//}
+//catch (Exception ex)
+//{
+//    Console.Write(ex.Message);
+//}
+
+//try
+//{
+//    var listadeClientes = clienteController.ListarTodosClientes();
+
+//    foreach (var clientedaLista in listadeClientes)
+//    {
+//        Console.WriteLine(clientedaLista);
+//    }
+//}
+//catch (Exception ex)
+//{
+//    Console.Write(ex.Message);
+//}
+
+clienteController.AtualizarTelefoneCliente("99999-9999", "x@urso.com");
+Console.WriteLine(clienteController.BuscaClientePorEmail("x@urso.com"));
+
+
